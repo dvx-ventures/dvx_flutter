a66c65e92ac29db5f484ec6dc4a42ff18ae5ffad
Steve Gehrman <sgehrman@gmail.com>
2021-07-05 14:18:37 -0700

    refactor

--- lib/src/hive_db/hive_data.dart
+++ lib/src/hive_db/hive_data.dart
@@ -1,8 +1,5 @@
 import 'package:hive/hive.dart';
 
-// flutter packages pub run build_runner build
-part 'hive_data.g.dart';
-
 @HiveType(typeId: 0)
 class HiveData extends HiveObject {
   HiveData({required this.name, required this.json, required this.date});
@@ -15,4 +12,41 @@ class HiveData extends HiveObject {
 
   @HiveField(2)
   final String? json;
+}
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+// generated TypeAdapter
+// flutter packages pub run build_runner build
+
+class HiveDataAdapter extends TypeAdapter<HiveData> {
+  @override
+  final int typeId = 0;
+
+  @override
+  HiveData read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return HiveData(
+      name: fields[0] as String?,
+      json: fields[2] as String?,
+      date: fields[1] as DateTime?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, HiveData obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.date)
+      ..writeByte(2)
+      ..write(obj.json);
+  }
 }
